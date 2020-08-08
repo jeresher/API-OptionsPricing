3e46fe5365981955866cbbd95ebea921c649774a
Jere Sher <jeresher@g.ucla.edu>
2020-08-07 23:23:35 -0700

    added input validation & refactored code

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
 const express = require('express');
-const Joi = require('joi');
 const app = express();
 const blackScholes = require('./models/blackScholes.js');
 
 app.use(express.json());
 
-app.get("/valuation/blackscholes/:type", (req, res) => {
-    const test = blackScholes(req.body, req.params.type);
-    res.send(test);
+app.get("/valuation/blackscholes", (req, res) => {
+    const value = blackScholes(req.body, res);
+    res.send(value);
 })
 
 app.listen(3000);
--- models/blackScholes.js
+++ models/blackScholes.js
@@ -1,21 +1,23 @@
+const Joi = require('joi');
 const { pow, log, sqrt, exp } = require('mathjs');
 const { cdf } = require('../tools/helperfunctions');
 
 
+function blackScholesModel(request, res) {
 
-function blackScholesModel(body, type) {
-    /*
-    const S = body.stockPrice
-    const K = body.strikePrice
-    const r = body.riskFreeInterestRate
-    const t = body.timeToMaturity
-    const v = body.volatility
-    */
-   const S = 10
-   const K = 10
-   const v = .30
-   const r = .04
-   const t = 1
+    const data = validateRequest(request);
+
+    if (data.error) {
+        res.status(400).send(data.error.details[0].message);
+        return;
+    }
+
+    const type = request.optionType
+    const S = request.stockPrice
+    const K = request.strikePrice
+    const r = request.riskFreeInterestRate
+    const t = request.timeToMaturity
+    const v = request.volatility
 
     const d1 = (log(S/K)+(r+(pow(v, 2)/2)*t))/(v*sqrt(t));
     const d2 = d1-(v*sqrt(t));
@@ -23,7 +25,23 @@ function blackScholesModel(body, type) {
     const c = S*cdf(d1)-K*exp(-r*t)*cdf(d2);
     const p = K*exp(-r*t)*cdf(-d2)-S*cdf(-d1);
 
-    return body;
+    if (type === "call") return String(c);
+    if (type === "put") return String(p);
+}
+
+
+function validateRequest(request) {
+
+    const schema = Joi.object({
+        optionType: Joi.string().valid("call", "put").required(),
+        stockPrice: Joi.number().required(),
+        strikePrice: Joi.number().required(),
+        riskFreeInterestRate: Joi.number().required(),
+        timeToMaturity: Joi.number().required(),
+        volatility: Joi.number().required()
+    })
+
+    return schema.validate(request);
 }
 
 module.exports = blackScholesModel
