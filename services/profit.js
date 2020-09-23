22d87f5ae2927ae1a13f1877ee3b2a88dc701091
Jere Sher <jeresher@g.ucla.edu>
2020-09-23 08:41:51 -0700

    added position and contract size

--- services/profit.js
+++ services/profit.js
@@ -8,6 +8,50 @@ function optionPayoff(req, res) {
 
         const schema = Joi.object({
             optionType: Joi.string().valid("call", "put").required(),
+            initialPrice: Joi.number().required(),                     // Initial price of the option
+            strikePrice: Joi.number().required(),                      // Strike price of the option
+            underlyingPrice: Joi.number().required(),                  // Projected price of the option
+            contractSize: Joi.number(),                                // Number of shares a contract represents (Default: 100)
+            positionSize: Joi.number()                                 // Number of contracts being held (Default: 1)
+        })
+    
+        return schema.validate(request);
+    }
+
+    const data = validateRequest(req.body);
+
+    if (data.error) {
+        res.status(400).send(data.error.details[0].message);
+        return;
+    }
+
+    const type = req.body.optionType
+    const I = req.body.initialPrice
+    const K = req.body.strikePrice
+    const U = req.body.underlyingPrice
+    const cSize = req.body.contractSize || 100
+    const pSize = req.body.positionSize || 1
+
+
+    const c = {
+        ProfitLossPerShare: max(U-K, 0) - I,
+        ProfitLossTotal: (max(U-K, 0) - I) * cSize * pSize
+    }
+    const p = {
+        ProfitLossPerShare: max(K-U, 0) - I,
+        ProfitLossTotal: (max(K-U, 0) - I) * cSize * pSize
+    }
+
+    if (type === "call") res.send(c);
+    if (type === "put") res.send(p);
+}
+
+function shortPayoff(req, res) {
+
+    function validateRequest(request) {
+
+        const schema = Joi.object({
+            optionType: Joi.string().valid("call", "put").required(),
             initialPrice: Joi.number().required(),
             strikePrice: Joi.number().required(),
             underlyingPrice: Joi.number().required()
@@ -28,11 +72,6 @@ function optionPayoff(req, res) {
     const K = req.body.strikePrice
     const U = req.body.underlyingPrice
 
-    const c = max(U-K, 0) - I;
-    const p = max(K-U, 0) - I;
-
-    if (type === "call") res.send(String(c));
-    if (type === "put") res.send(String(p));
 }
 
 module.exports = {
