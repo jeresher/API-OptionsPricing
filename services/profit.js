c3cd10a33de8329764da57859a752e813e2f39e9
Jere Sher <jeresher@g.ucla.edu>
2020-09-23 09:27:36 -0700

    added direction to option payoff

--- services/profit.js
+++ services/profit.js
@@ -8,7 +8,8 @@ function longOptionPayoff(req, res) {
 
         const schema = Joi.object({
             optionType: Joi.string().valid("call", "put").required(),
-            initialPrice: Joi.number().required(),                     // Initial price of the option
+            direction: Joi.string().valid("long", "short"),            // The direction, long or short, of the option (Default: long)
+            initialPrice: Joi.number().required(),                     // Initial price, or premium, of the option
             strikePrice: Joi.number().required(),                      // Strike price of the option
             underlyingPrice: Joi.number().required(),                  // Projected price of the option
             contractSize: Joi.number(),                                // Number of shares a contract represents (Default: 100)
@@ -26,52 +27,34 @@ function longOptionPayoff(req, res) {
     }
 
     const type = req.body.optionType
+    const direction = req.body.direction || "long"
     const I = req.body.initialPrice
     const K = req.body.strikePrice
     const U = req.body.underlyingPrice
     const cSize = req.body.contractSize || 100
     const pSize = req.body.positionSize || 1
 
-
     const c = {
-        ProfitLossPerShare: max(U-K, 0) - I,
-        ProfitLossTotal: (max(U-K, 0) - I) * cSize * pSize
+        ProfitLossPerShare: (direction === "long") ? 
+            max(U-K, 0) - I : 
+            (max(U-K, 0) - I) * -1 
+        ,
+        ProfitLossTotal: (direction === "long") ? 
+            (max(U-K, 0) - I) * cSize * pSize :
+            ((max(U-K, 0) - I) * cSize * pSize) * -1
     }
     const p = {
-        ProfitLossPerShare: max(K-U, 0) - I,
-        ProfitLossTotal: (max(K-U, 0) - I) * cSize * pSize
+        ProfitLossPerShare: (direction === "long") ? 
+            max(K-U, 0) - I :
+            (max(K-U, 0) - I) * -1
+        ,
+        ProfitLossTotal: (direction === "long") ?
+            (max(K-U, 0) - I) * cSize * pSize :
+            ((max(K-U, 0) - I) * cSize * pSize) * -1
     }
 
     if (type === "call") res.send(c);
     if (type === "put") res.send(p);
-}
-
-function shortOptionPayoff(req, res) {
-
-    function validateRequest(request) {
-
-        const schema = Joi.object({
-            optionType: Joi.string().valid("call", "put").required(),
-            initialPrice: Joi.number().required(),
-            strikePrice: Joi.number().required(),
-            underlyingPrice: Joi.number().required()
-        })
-    
-        return schema.validate(request);
-    }
-
-    const data = validateRequest(req.body);
-
-    if (data.error) {
-        res.status(400).send(data.error.details[0].message);
-        return;
-    }
-
-    const type = req.body.optionType
-    const I = req.body.initialPrice
-    const K = req.body.strikePrice
-    const U = req.body.underlyingPrice
-
 }
 
 module.exports = {
