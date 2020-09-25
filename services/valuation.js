237a070193f8ce1fb9ff9d017eb26f124fcc64af
Jere Sher <jeresher@g.ucla.edu>
2020-09-25 12:18:12 -0700

    moved underlyingPriceTree and optionPriceTree from valuations.js to helperfunctions.js

--- services/valuation.js
+++ services/valuation.js
@@ -1,6 +1,6 @@
 const Joi = require('joi');
 const { pow, log, sqrt, exp, max, sign } = require('mathjs');
-const { cdf } = require('../tools/helperfunctions');
+const { cdf, getUnderlyingPriceTree, getOptionPriceTree } = require('../tools/helperfunctions');
 
 
 function blackScholesModel(req, res) {
@@ -41,71 +41,6 @@ function blackScholesModel(req, res) {
 
     if (type === "call") res.send(String(c));
     if (type === "put") res.send(String(p));
-}
-
-function getUnderlyingPriceTree({undPrice, steps, upMove, downMove}) {
-
-    var tree = [[undPrice]];
-
-    for (let i=1; i <= steps; i++) {
-        var step = [];
-        var previousStep = tree[i-1];
-
-        step.push(previousStep[0] * upMove);
-
-        for (let i=0; i < previousStep.length; i++) {
-            step.push(previousStep[i] * downMove)
-        }
-
-        tree.push(step);
-    }
-
-    return tree;
-}
-
-function getOptionPriceTree(underlyingPriceTree, {stepDiscount, optionType, isEuro, strike, upProb, downProb}) {
-    
-    var tree = [];
-    
-    // Last step in Option Price Tree
-    const lastStep = underlyingPriceTree[underlyingPriceTree.length-1];
-    var nodes = [];
-
-    for (let i=0; i < lastStep.length; i++) {
-        const price = lastStep[i];
-        
-        if (optionType === "call") nodes.push(max(0, price-strike));
-        if (optionType === "put") nodes.push(max(0, strike-price));
-    }
-
-    tree.push(nodes);
-
-    // Remaining steps in Option Price Tree
-    for (let i=0; i < underlyingPriceTree.length - 1; i++) {
-        const step = tree[i];
-        const undTreeStep = underlyingPriceTree[underlyingPriceTree.length-2-i]
-        var nodes = [];
-
-        for (let i=0; i < step.length-1; i++) {
-            const upPrice = step[i];
-            const downPrice = step[i+1];
-            const undPrice = undTreeStep[i];
-
-            const expectedValue = (upPrice * upProb + downPrice * downProb) * stepDiscount;
-            const intrinsicValue = () => {
-                if (optionType === "call") return undPrice - strike;
-                if (optionType === "put") return strike - undPrice;
-            }
-
-            const price = isEuro ? expectedValue : max(expectedValue, intrinsicValue())
-            
-            nodes.push(price);
-        }
-
-        tree.push(nodes);
-    }
-
-    return tree.reverse();
 }
 
 function coxRossRubinsteinModel(req, res) {
--- tools/helperfunctions.js
+++ tools/helperfunctions.js
@@ -1,4 +1,4 @@
-const { erf } = require('mathjs');
+const { erf, max } = require('mathjs');
 
 const helperfunctions = {
 
@@ -7,6 +7,71 @@ const helperfunctions = {
         if (standardDeviation == null) standardDeviation = 1;
         
         return (1 - erf((mean - x ) / (Math.sqrt(2) * standardDeviation))) / 2
+    },
+
+    getUnderlyingPriceTree: ({undPrice, steps, upMove, downMove}) => {
+
+        var tree = [[undPrice]];
+    
+        for (let i=1; i <= steps; i++) {
+            var step = [];
+            var previousStep = tree[i-1];
+    
+            step.push(previousStep[0] * upMove);
+    
+            for (let i=0; i < previousStep.length; i++) {
+                step.push(previousStep[i] * downMove)
+            }
+    
+            tree.push(step);
+        }
+    
+        return tree;
+    },
+
+    getOptionPriceTree: (underlyingPriceTree, {stepDiscount, optionType, isEuro, strike, upProb, downProb}) => {
+    
+        var tree = [];
+        
+        // Last step in Option Price Tree
+        const lastStep = underlyingPriceTree[underlyingPriceTree.length-1];
+        var nodes = [];
+    
+        for (let i=0; i < lastStep.length; i++) {
+            const price = lastStep[i];
+            
+            if (optionType === "call") nodes.push(max(0, price-strike));
+            if (optionType === "put") nodes.push(max(0, strike-price));
+        }
+    
+        tree.push(nodes);
+    
+        // Remaining steps in Option Price Tree
+        for (let i=0; i < underlyingPriceTree.length - 1; i++) {
+            const step = tree[i];
+            const undTreeStep = underlyingPriceTree[underlyingPriceTree.length-2-i]
+            var nodes = [];
+    
+            for (let i=0; i < step.length-1; i++) {
+                const upPrice = step[i];
+                const downPrice = step[i+1];
+                const undPrice = undTreeStep[i];
+    
+                const expectedValue = (upPrice * upProb + downPrice * downProb) * stepDiscount;
+                const intrinsicValue = () => {
+                    if (optionType === "call") return undPrice - strike;
+                    if (optionType === "put") return strike - undPrice;
+                }
+    
+                const price = isEuro ? expectedValue : max(expectedValue, intrinsicValue())
+                
+                nodes.push(price);
+            }
+    
+            tree.push(nodes);
+        }
+    
+        return tree.reverse();
     }
 
 }
