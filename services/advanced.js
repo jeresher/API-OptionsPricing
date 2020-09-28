03d5083b5d1fa7472e9d8507fc2718115049b92c
Jere Sher <jeresher@g.ucla.edu>
2020-09-28 13:40:20 -0700

    completed iron butterfly strategy

--- services/advanced.js
+++ services/advanced.js
@@ -432,11 +432,132 @@ function ironCondor(req, res) {
     })
 }
 
+function ironButterfly(req, res) {
+
+    function validateRequest(request) {
+        
+        const schema = Joi.object({
+            longPutStrikePrice: Joi.number().required(),
+            longPutPremiumPrice: Joi.number().required(),
+            shortPutStrikePrice: Joi.number().required(),               // Strike price of the short put
+            shortPutPremiumPrice: Joi.number().required(),              // Premium price of the short put
+            shortCallStrikePrice: Joi.number().required(),              // Strike price of the short call
+            shortCallPremiumPrice: Joi.number().required(),             // Premium price of the short call
+            longCallStrikePrice: Joi.number().required(),
+            longCallPremiumPrice: Joi.number().required(),
+            underlyingPrice: Joi.number().required(),                   // Market price of the asset
+            contractSize: Joi.number(),                                 // Number of shares a contract represents (Default: 100)
+            positionSizeEachOptionType: Joi.number()                    // Number of contracts being held per Option Type (Default: 1)
+        })
+
+        return schema.validate(request);
+    }
+
+    const data = validateRequest(req.body);
+
+    if (data.error) {
+        res.status(400).send(data.error.details[0].message);
+        return;
+    }
+
+    const longPutK = req.body.longPutStrikePrice
+    const longPutI = req.body.longPutPremiumPrice
+    const shortPutK = req.body.shortPutStrikePrice
+    const shortPutI = req.body.shortPutPremiumPrice
+    const shortCallK = req.body.shortCallStrikePrice
+    const shortCallI = req.body.shortCallPremiumPrice
+    const longCallK = req.body.longCallStrikePrice
+    const longCallI = req.body.longCallPremiumPrice
+    const U = req.body.underlyingPrice
+    const cSize = req.body.contractSize || 100
+    const pSize = req.body.positionSizeEachOptionType || 1
+
+
+    const initialLongPutCF = -(longPutI * cSize * pSize);
+    const initialShortPutCF = shortPutI * cSize * pSize;
+    const initialShortCallCF = shortCallI * cSize * pSize;
+    const initialLongCallCF = -(longCallI * cSize * pSize);
+
+    const longPutValue = () => {
+        if (U > longPutK) return 0;
+        else return (longPutK - U) * cSize * pSize;
+    };
+    const shortPutValue = () => {
+        if (U > shortPutK) return 0;
+        else return -((shortPutK - U) * cSize * pSize);
+    };
+    const shortCallValue = () => {
+        if (U < shortCallK) return 0;
+        else return -((U - shortCallK) * cSize * pSize);
+    };
+    const longCallValue = () => {
+        if (U < longCallK) return 0;
+        else return (U - longCallK) * cSize * pSize;
+    };
+
+    const longPutProfitLoss = longPutValue() + initialLongPutCF;
+    const shortPutProfitLoss = shortPutValue() + initialShortPutCF;
+    const shortCallProfitLoss = shortCallValue() + initialShortCallCF;
+    const longCallProfitLoss = longCallValue() + initialLongCallCF;
+
+    const longPutLeg = {
+        direction: "long",
+        type: "put",
+        strike: longPutK,
+        size: pSize,
+        initialPrice: longPutI,
+        initialCF: initialLongPutCF,
+        value: longPutValue(),
+        profitLoss: longPutProfitLoss
+    }
+    const shortPutLeg = {
+        direction: "short",
+        type: "put",
+        strike: shortPutK,
+        size: pSize,
+        initialPrice: shortPutI,
+        initialCF: initialShortPutCF,
+        value: shortPutValue(),
+        profitLoss: shortPutProfitLoss
+    }
+    const shortCallLeg = {
+        direction: "short",
+        type: "call",
+        strike: shortCallK,
+        size: pSize,
+        initialPrice: shortCallI,
+        initialCF: initialShortCallCF,
+        value: shortCallValue(),
+        profitLoss: shortCallProfitLoss
+    }
+    const longCallLeg = {
+        direction: "long",
+        type: "call",
+        strike: longCallK,
+        size: pSize,
+        initialPrice: longCallI,
+        initialCF: initialLongCallCF,
+        value: longCallValue(),
+        profitLoss: longCallProfitLoss
+    }
+
+    res.send({
+        longPutLeg,
+        shortPutLeg,
+        shortCallLeg,
+        longCallLeg,
+        initialCF: initialLongPutCF + initialShortPutCF + initialShortCallCF + initialLongCallCF,
+        value: longPutValue() + shortPutValue() + shortCallValue() + longCallValue(),
+        profitLoss: longPutProfitLoss + shortPutProfitLoss + shortCallProfitLoss + longCallProfitLoss
+    })
+}
+
 
 module.exports = {
     longStraddle,
     longStrangle,
     shortStraddle,
     shortStrangle,
-    ironCondor
+    ironCondor,
+    ironButterfly
 }
